2866509402706be5211a36d4890154e95f6718ad
Mathias Pereira <mattdu94310@hotmail.fr>
2016-03-10 14:36:10 +0100

    Exercice de mattdu94310 aka mathias V2

--- mattdu94310/src/main/java/fr/dauphine/lamsade/hib/ads/Test.java
+++ mattdu94310/src/main/java/fr/dauphine/lamsade/hib/ads/Test.java
@@ -3,10 +3,16 @@ package fr.dauphine.lamsade.hib.ads;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Test {
+	
+	private static Logger myLogger = Logger.getLogger("fr.dauphine.lamsade.hib.ads");
+	
 	public static void main(String[] args) throws Exception {
+		myLogger.log(Level.INFO, "start");
 		Class.forName("org.h2.Driver");
 		Connection connection = DriverManager.getConnection("jdbc:h2:~/test","sa","");
 		Driver d = DriverManager.getDriver("jdbc:h2:~/test");
