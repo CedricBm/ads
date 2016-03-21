df2623ceaa6b7d9a3aca2cf148cc772518c835f1
Yassine <Yassine>
2016-03-21 23:56:25 +0100

    add loggers

--- src/main/java/fr/dauphine/lamsade/hib/ads/forms/FootballerForm.java
+++ src/main/java/fr/dauphine/lamsade/hib/ads/forms/FootballerForm.java
@@ -3,14 +3,16 @@ package main.java.fr.dauphine.lamsade.hib.ads.forms;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import main.java.fr.dauphine.lamsade.hib.ads.beans.Footballer;
+import main.java.fr.dauphine.lamsade.hib.ads.dao.UserDao;
 import main.java.fr.dauphine.lamsade.hib.ads.resources.Util;
 
 public class FootballerForm {
 
-	
+	  private static final Logger LOGGER = Logger.getLogger(UserDao.class.getCanonicalName());	
 	public Footballer getFootballer(HttpServletRequest request){
 		
 		Footballer f = new Footballer();
@@ -29,6 +31,7 @@ public class FootballerForm {
 			java.sql.Date sql = new java.sql.Date(parsed.getTime());
 		    f.setBirthdate(sql);
 		} catch (ParseException e) {
+		    LOGGER.severe("Error while trying parse information from request footballers: " + e);
 			return null;
 		}
 		f.setNbGamesInternational(Integer.parseInt(Util.getInputValue(request, "nb_games_international")));
