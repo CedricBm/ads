e5c062f14f7c5b6caec9b6c10e0750a82de60aab
inakiclzd <inaki.calzada@hotmail.fr>
2016-04-13 19:38:15 +0200

    change schema generation property in persistence.xml and footballer modify redirection

--- src/main/java/fr/dauphine/lamsade/hib/ads/servlets/FootballerModifyServlet.java
+++ src/main/java/fr/dauphine/lamsade/hib/ads/servlets/FootballerModifyServlet.java
@@ -20,29 +20,37 @@ import main.java.fr.dauphine.lamsade.hib.ads.forms.FootballerForm;
  */
 @WebServlet("/footballers/modify")
 public class FootballerModifyServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
-  private static final Logger LOGGER = Logger.getLogger(UserServlet.class.getCanonicalName());
-  @EJB
-  private FootballerDao fd;
-  @EJB
-  private FootballerForm ff;
-  
-  public FootballerModifyServlet() throws NamingException {
-    super();
-  }
-  
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    Footballer f = fd.find(Integer.parseInt(request.getParameter("id")));
-    request.setAttribute("footballer", f);
-    LOGGER.info("GET /footballers/modify with id: " + f.getId());
-    this.getServletContext().getRequestDispatcher("/WEB-INF/footballers/edit.jsp").forward(request, response);
-  }
-  
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    Footballer f = ff.getFootballerForEdit(request);
-    fd.save(f);
-    LOGGER.info("POST /footballers/modify with id: " + f.getId());
-    doGet(request, response);
-  }
-  
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = Logger.getLogger(UserServlet.class.getCanonicalName());
+	@EJB
+	private FootballerDao fd;
+	@EJB
+	private FootballerForm ff;
+
+	public FootballerModifyServlet() throws NamingException {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Footballer f = fd.find(Integer.parseInt(request.getParameter("id")));
+		request.setAttribute("footballer", f);
+		LOGGER.info("GET /footballers/modify with id: " + f.getId());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/footballers/edit.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Footballer f = ff.getFootballerForEdit(request);
+		if (f != null) {
+			fd.save(f);
+			request.setAttribute("error", null);
+			LOGGER.info("POST /footballers/modify with id: " + f.getId());
+			response.sendRedirect("../footballers");
+		} else {
+			request.setAttribute("error", 1);
+			doGet(request, response);
+		}
+	}
+
 }
